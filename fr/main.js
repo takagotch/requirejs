7f8378c76749fb1f1628f87c302053c6ce03bbca
takagotch <dyaccb@gmail.com>
2018-02-10 10:26:20 +0900

    require(['util', 'minus'], function(util, minus){ console.log(); })

--- fr/main.js
+++ fr/main.js
@@ -3,3 +3,14 @@ require(['util'], function(util){
   console.log('10 + 21 = ' + result);
 });
 
+require([], function(minus){
+  var result = minus(22, 14);
+  console.log('22 - 14 = ' + result);
+});
+
+require([], function(util, minus){
+  console.log('14 + 51 = ' + util.add(14, 51));
+  console.log('22 - 14 = ' + minus(22, 14));
+});
+
+
